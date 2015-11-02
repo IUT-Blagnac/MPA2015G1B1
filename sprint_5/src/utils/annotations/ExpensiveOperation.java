d251c70125b84cdc5f85d77e5231d25f29b02267
Nicolas-Calvet <nicolas.calvet@etu.univ-tlse2.fr>
2015-11-02 18:22:08 +0100

    Update our project

--- sprint_5/src/utils/annotations/ExpensiveOperation.java
+++ sprint_5/src/utils/annotations/ExpensiveOperation.java
@@ -1,4 +1,4 @@
-package utils.annotations;
+﻿package utils.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
--- sprint_5/src/view/JPanelGroup.java
+++ sprint_5/src/view/JPanelGroup.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Collection;
 import java.util.Iterator;
-
+ 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
